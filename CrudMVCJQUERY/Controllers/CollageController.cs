f7255551465b2c932db54346db67fddae22e4108
Vijay9369757210 <prashantinfo2022@gmail.com>
2026-07-31 01:43:35 +0530

    crud insert showAdd .gitattributes and .gitignore.alll fil insert

---
+++ CrudMVCJQUERY/Controllers/CollageController.cs
@@ -0,0 +1,116 @@
+﻿using CRUDWITHModelViewController.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDWITHModelViewController.Controllers
+{
+    public class CollageController : Controller
+    {
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;initial catalog=dbCRUDEddlJuly28_26;integrated security=true");
+
+        public ActionResult CollageForm()
+        {
+            return View();
+        }
+        public JsonResult bindcountries()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from countries", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            string countries = JsonConvert.SerializeObject(dt);
+            return Json(countries, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult bindstates()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from states", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            string states = JsonConvert.SerializeObject(dt);
+            return Json(states, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        public void InsertUpdateData(CollageClass obj)
+        {
+            if (obj.Id == 0)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("proctblmanager", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("action", "insert");
+                cmd.Parameters.AddWithValue("@name", obj.Name);
+                cmd.Parameters.AddWithValue("@country", obj.Countries);
+                cmd.Parameters.AddWithValue("@state", obj.States);
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+            }
+            else
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("proctblmanager", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("action", "update");
+                cmd.Parameters.AddWithValue("@id", obj.Id);
+                cmd.Parameters.AddWithValue("@name", obj.Name);
+                cmd.Parameters.AddWithValue("@country", obj.Countries);
+                cmd.Parameters.AddWithValue("@state", obj.States);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                //hello
+            }
+        }
+        public JsonResult ShowData()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblmanager", con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            string data = JsonConvert.SerializeObject(dt);
+            return Json(data, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public void DeleteData(CollageClass obj)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from tblmanager where id = @id", con);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.AddWithValue("@id", obj.Id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public JsonResult EditData(CollageClass obj)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from tblmanager where id=@id", con);
+            cmd.Parameters.AddWithValue("@id", obj.Id);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Close();
+            string data = JsonConvert.SerializeObject(dt);
+            return Json(data, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+
+}
