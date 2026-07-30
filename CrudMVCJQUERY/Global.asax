e930a86f81c709bcf63fd1ddbe5b41eb9e910da9
Vijay9369757210 <prashantinfo2022@gmail.com>
2026-07-31 01:43:35 +0530

    alll fil insert

---
+++ CrudMVCJQUERY/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDWITHModelViewController.MvcApplication" Language="C#" %>
---
+++ CrudMVCJQUERY/Models/CollageClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUDWITHModelViewController.Models
+{
+    public class CollageClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }//get mens data get bhi aur data set donokarshakte hai
+        public int Countries { get; set; }
+        public int States { get; set; }
+    }
+}
